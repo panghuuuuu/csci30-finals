6a91966fcf10f1f53a342c1dcffcfeb0070148c9
Andre Dalwin <andudtan@gmail.com>
2022-12-03 15:52:35 +0800

    Added energy() and updated project files with tester

--- seamcarving_files/seamcarver.py
+++ seamcarving_files/seamcarver.py
@@ -8,7 +8,22 @@ class SeamCarver(Picture):
         '''
         Return the energy of pixel at column i and row j
         '''
-        raise NotImplementedError
+
+        if i < self.width() and j < self.height():
+            Rx = abs(self[i-1,j][0] - self[i+1,j][0])
+            Gx = abs(self[i-1,j][1] - self[i+1,j][1])
+            Bx = abs(self[i-1,j][2] - self[i+1,j][2])
+            dX = ((Rx**2) + (Gx**2) + (Bx**2))
+
+            Ry = abs(self[i,j-1][0] - self[i,j+1][0])
+            Gy = abs(self[i,j-1][1] - self[i,j+1][1])
+            By = abs(self[i,j-1][2] - self[i,j+1][2])
+            dY = ((Ry**2) + (Gy**2) + (By**2))
+
+            return (dX+dY)
+        else: #If selected pixel is out of bounds
+            raise IndexError("Selected pixel is out of bounds")
+            
 
     def find_vertical_seam(self) -> list[int]:
         '''
