d26a494458b69a3dc45ddaeff6f1eb316b19d3ae
panghuuuuu <ysabella.panghulan@obf.ateneo.edu>
2022-12-05 23:00:22 +0800

    edit-hori

--- seamcarving_files/seamcarver.py
+++ seamcarving_files/seamcarver.py
@@ -35,37 +35,37 @@ class SeamCarver(Picture):
         
 
         # Create a matrix with the cumulative sum
-        pixel_matrix = [[self.energy(i, 0) for i in range(width)]]
+        energy_matrix = [[self.energy(i, 0) for i in range(width)]]
         for j in range(1, height):
             row = []
             for i in range(width):
                 # Leftmost column
                 if i == 0:
-                    neighbors = [pixel_matrix[j-1][0], pixel_matrix[j-1][1]]
+                    neighbors = [energy_matrix[j-1][0], energy_matrix[j-1][1]]
                     row.append(self.energy(0, j) + min(neighbors))
 
                 # Center columns
                 elif i > 0 and i < width-1:
-                    neighbors = [pixel_matrix[j-1][i-1], pixel_matrix[j-1][i], pixel_matrix[j-1][i+1]]
+                    neighbors = [energy_matrix[j-1][i-1], energy_matrix[j-1][i], energy_matrix[j-1][i+1]]
                     row.append(self.energy(i, j) + min(neighbors))
 
                 # Rightmost column
                 else:
-                    neighbors = [pixel_matrix[j-1][i-1], pixel_matrix[j-1][i]]
+                    neighbors = [energy_matrix[j-1][i-1], energy_matrix[j-1][i]]
                     row.append(self.energy(width-1, j) + min(neighbors))
-            pixel_matrix.append(row)
+            energy_matrix.append(row)
 
         # Last row
-        prev_index = pixel_matrix[-1].index(min(pixel_matrix[-1]))
+        prev_index = energy_matrix[-1].index(min(energy_matrix[-1]))
         indexes = [prev_index]
         for j in range(height-2, -1, -1):
             j1, j2 = prev_index-1, prev_index+2
             if prev_index > 0 and prev_index < width-1:
-                neighbors = pixel_matrix[j][j1:j2]
+                neighbors = energy_matrix[j][j1:j2]
             elif prev_index == 0:
-                neighbors = [sys.maxsize] + pixel_matrix[j][:2]
+                neighbors = [sys.maxsize] + energy_matrix[j][:2]
             else:
-                neighbors = pixel_matrix[j][j1:]
+                neighbors = energy_matrix[j][j1:]
             prev_index += neighbors.index(min(neighbors))-1
             indexes.append(prev_index)
         return indexes[::-1]
@@ -77,16 +77,16 @@ class SeamCarver(Picture):
         '''
         sc = SeamCarver(Picture.picture(self).transpose(Image.ROTATE_90))
         return sc.find_vertical_seam()[::-1] 
-         
+
     def remove_vertical_seam(self, seam: list[int]):
         '''
         Remove a vertical seam from the picture
         '''
-        width = Picture.width(self)
-        height = Picture.height(self) 
+        width = self._width
+        height = self._height
         seam_len = len(seam)
-        if self._width  == 1:
-            raise SeamError("Can't shrink the image vertically")
+        if width == 1:
+            raise SeamError("Can't shrink the image horizontally")
         elif self.check_invalid_seam(seam) == False:
             raise SeamError("Invalid seam")
         elif height != seam_len:
@@ -94,7 +94,7 @@ class SeamCarver(Picture):
         else: 
             for j in range(height):
                 for i in range(seam[j], width-1):
-                    self[i, j] = self[i+1, j]
+                    self[i, j] = self[i + 1, j]
                 del self[width-1, j]
             self._width -= 1
 
@@ -102,10 +102,10 @@ class SeamCarver(Picture):
         '''
         Remove a horizontal seam from the picture
         '''
-        width = Picture.width(self)
-        height = Picture.height(self)
+        width = self._width
+        height = self._height
         seam_len = len(seam)
-    
+        
         if height == 1:
             raise SeamError("Can't shrink the image horizontally")
         elif self.check_invalid_seam(seam) == False:
@@ -113,18 +113,16 @@ class SeamCarver(Picture):
         elif width != seam_len:
             raise SeamError("Attempted to remove seam with wrong length")
         else: 
-            for i in range(width):
-                #Find which column times row to be deleted based on the seam
-                j = seam[i]
-                while j < height - 1:
-                    self[i, j] = self[i, j + 1]
-                    j += 1
+            sc = SeamCarver(self.picture().rotate(90, expand=True))
+            sc.remove_vertical_seam(seam[::-1])
 
-            for i in range(width):
-                del self[i, height - 1]
+            self._width = sc.height()
+            self._height = sc.width()
+            self.clear()
+            for j in range(self._height):
+                for i in range(self._width):
+                    self[i, j] = sc[j, (sc._height-1) - i]
 
-            self._height -= 1
-    
     def check_invalid_seam(self, seam: list[int]):
         for i in range(len(seam)-1):
             x = abs(seam[i] - seam[i+1])
